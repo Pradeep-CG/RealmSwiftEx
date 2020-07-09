1720255524177bdefd474d304fc06cd9e347711d
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2020-07-09 22:53:34 +0530

    Initial Commit

---
+++ RealmSwiftEx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmSwiftEx
+//
+//  Created by Pradeep on 09/07/20.
+//  Copyright © 2020 Pradeep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
